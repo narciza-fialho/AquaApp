9fbb719cac7ae64f00f0d359f56f4d49e53b4658
narciza-fialho <narciza.fialho@hotmail.com>
2022-10-23 15:55:47 -0400

    Adicionar arquivos de projeto.

---
+++ AquaApp/AquaApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AquaApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AquaApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ AquaApp/AquaApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AquaApp.Models;
+using AquaApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AquaApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
